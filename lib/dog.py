325130c3c53932ba952418656a0d44da5a359907
Historia Montague <historia.montague@outlook.com>
2023-06-12 11:39:19 -0600

    Finished Lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,24 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name.like(name)).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id.is_(id)).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.breed.is_(breed),
+                                     Dog.name.like(name)).first()
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).filter(Dog.name.is_(dog.name)).update({Dog.breed: breed})
